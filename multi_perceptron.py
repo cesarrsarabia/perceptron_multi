e1e126f19301cf584c89d5fdc6866e46073248d0
Cesar Sarabia <cesar.rsarabia@alumnos.udg.mx>
2020-02-19 22:18:10 -0600

    Commit 2 Multiples neuronas

--- multi_perceptron.py
+++ multi_perceptron.py
@@ -9,14 +9,44 @@ import time
 import numpy as np
 
 
-def grafica_lineas(this_w,last_plot):
+def grafica_puntos():
+    colores = ['*b','*g','pm','or','+k']
+    aux_arr = []
+    xlim([-2,3])
+    ylim([-2,3])
+    last_plot=-1
+    for cont,this_element in enumerate(salidas_deseadas):
+        str_salida = '-'.join(str(e) for e in this_element)
+        if str_salida not in aux_arr:
+            last_plot+=1
+            aux_arr.append(str_salida)
+            x_1 = datos_entradas[cont]
+            ax.plot(x_1[0],x_1[1], colores[cont])
+    return last_plot
+
+    
+    
+def grafica_lineas(this_w,last_plot,is_last_plot):
     clases = ['--k','--b','--r','--y','--g']
-    for c,item in enumerate(this_w):
-        ax.plot([-2,3],[(-item[0]/item[1]) *
-        (-2)-(b[c]/item[1]), (-item[0]/item[1])*(3)-(b[c]/item[1])],clases[c])
-        plt.pause(0.01)
-    for c in range(1,num_col_salidas):
-        ax.lines.pop(last_plot + c)
+    if not is_last_plot:
+        index_plot = last_plot
+        for c,item in enumerate(this_w):
+            ax.plot([-2,3],[(-item[0]/item[1]) *
+            (-2)-(b[c]/item[1]), (-item[0]/item[1])*(3)-(b[c]/item[1])],clases[c])
+            plt.pause(0.000000000000000000000000000000000001)
+            index_plot += 1
+        for i in range(index_plot,last_plot,-1):
+            ax.lines.pop(i)
+    else:
+        plt.cla()
+        grafica_puntos()
+        for c,item in enumerate(this_w):
+            ax.plot([-2,3],[(-item[0]/item[1]) *
+            (-2)-(b[c]/item[1]), (-item[0]/item[1])*(3)-(b[c]/item[1])],clases[c])
+        
+        #plt.close('all')
+    #for c in range(1,num_col_salidas):
+    #   ax.lines.pop(last_plot + c)
 
 def funcion_escalon(x):
     if x<0:
@@ -36,11 +66,11 @@ def obtiene_Salida(r,c):
 #training set contiene entradas y salidas
 training_set = []
 
-
+#plt.ion()
 with open('entradas.txt') as f_1:
     datos_entradas = f_1.read().splitlines() 
 
-with open('salidas_deseadas.txt') as f_2:
+with open('salidas_deseadas_3.txt') as f_2:
     salidas_deseadas = f_2.read().splitlines() 
 
 #Convierte entrada a lista de enteros
@@ -59,7 +89,7 @@ num_col_salidas = len(salidas_deseadas[0]) - salidas_deseadas[0].count(' ')
 w = np.random.rand(num_col_salidas,2)
 errors = [] 
 eta = .5
-epoch = 40
+epoch = 8
 b = np.zeros(num_col_salidas)
 
 #crea el training set
@@ -76,6 +106,11 @@ for i in range(num_col_salidas):
     training_set.append(training_set_aux)
 
 fig, ax = plt.subplots()
+
+lst_i_plot = grafica_puntos()
+'''
+fig, ax = plt.subplots()
+
 xlim([-2,3])
 ylim([-2,3])
 
@@ -84,14 +119,14 @@ ax.plot(x1[0,0],x1[1,0], '*b')
 ax.plot(x1[0,1],x1[1,1], '*g')
 ax.plot(x1[0,2],x1[1,2], 'pm')
 ax.plot(x1[0,3],x1[1,3], 'or')
-
+'''
 
 #grafica primera linea
 #ax.plot([-2,3],[(-w[0][0]/w[0][1])*(-2)-(b/w[0][1]), (-w[0][0]/w[0][1])*(3)-(b/w[0][1])],'--g')
 #grafica segunda linea
 #ax.plot([-2,3],[(-w[1][0]/w[1][1])*(-2)-(b/w[1][1]), (-w[1][0]/w[1][1])*(3)-(b/w[1][1])],'--m')
 
-lst_i_plot = 3
+
 #print(w[0][0])
 #print(w[0][1])
 for i in range(epoch):
@@ -102,10 +137,12 @@ for i in range(epoch):
             for index, value in enumerate(x):          
                 w[count][index] += eta * error * value
                 b[count] += eta*error
-                grafica_lineas(w,lst_i_plot)
+                grafica_lineas(w,lst_i_plot,False)
                 
-
-
+print("PERCEPTRON ENTRENADOS")
+grafica_lineas(w,lst_i_plot,True)
+plt.show()
+#grafica_lineas(w,lst_i_plot,True)
 
 
 #plt.show()
